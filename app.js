4ba1061509f8856899f5b782ee56e4318d76518b
JRodriAlarc <jona10131313@gmail.com>
2025-03-18 16:50:29 -0600

    Feat(app.js) Añadir la lógica para sortear los amigos

--- app.js
+++ app.js
@@ -1,32 +1,56 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
-listaAmigos = document.getElementById('listaAmigos');
-resultado = document.getElementById('resultado')
+let listaAmigosSecretos = [];
+let cantidadAmigos = 0;
 
-
-listaAmigosSecretos = [];
+let listaAmigos = document.getElementById('listaAmigos');
+let resultado = document.getElementById('resultado');
 
 function agregarAmigo() {
-    console.log("addNew Amigo");
+    let nombreAmigo = document.getElementById('amigo').value.trim();
 
-    nombreAmigo = document.getElementById('amigo').value;
-
-    if (nombreAmigo ==! '') {
+    if (nombreAmigo !== '') {
+        //Verificar que el nombre no  inicie con número
+        //Verificar que no se repita en la lista de amigos
+        console.log("addNew Amigo");
         listaAmigosSecretos.push(nombreAmigo)
+        limpiarcampo();
     } else {
-        alert ("Debe Ingresar un Nombre")
+        alert ("Por favor, inserte un nombre")
     }
 
 }
 
 function sortearAmigo() {
     console.log("sortear Amigo");
+    
+    cantidadAmigos = listaAmigosSecretos.length;
 
-    for (let i = 0; i < listaAmigosSecretos.length; i++) {
-        console.log(listaAmigosSecretos[i]);
+    if (cantidadAmigos >= 2) {
+        let amigoAleatorio = Math.floor(Math.random()*cantidadAmigos);
+        
+        console.log(`Amigo Aleatorio: ${listaAmigosSecretos[amigoAleatorio]}`);
+
+        listaAmigosSecretos.splice(amigoAleatorio, 1);
+        mostarListaAmigos();
+    } 
+    
+    if (cantidadAmigos < 2) {
+        alert("Necesita un minimo de 2 amigos para sortear");
     }
 }
 
 function reiniciarJuego() {
     console.log("reiniciar Juego");
+}
+
+
+function limpiarcampo() {
+    document.getElementById('amigo').value = '';
+}
+
+function mostarListaAmigos () {
+    for (let i = 0; i < listaAmigosSecretos.length; i++) {
+        console.log(listaAmigosSecretos[i]);
+    }
 }
